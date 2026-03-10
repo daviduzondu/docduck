648e5f418fe488870602d5a0632388d1a6faca3c
David Uzondu <uzondudavid2004@gmail.com>
2026-03-10 15:24:53 +0100

    cleanup code

---
+++ apps/server/src/lib/hocuspocus.ts
@@ -0,0 +1,17 @@
+import { Hocuspocus, } from "@hocuspocus/server";
+import { Server } from "node:http";
+import { WebSocketServer } from 'ws';
+
+export function initializeHocuspocus(wss: WebSocketServer) {
+ const hocuspocus = new Hocuspocus({
+  onConnect(data) {
+   return new Promise((res) => res(undefined));
+  },
+ });
+
+ wss.on('connection', (ws, req) => {
+  hocuspocus.handleConnection(ws, req);
+  ws.on('error', console.error);
+ })
+}
+
