c84a7383fe2f708d40162e5c481cdd1f334bc65f
David Uzondu <uzondudavid2004@gmail.com>
2026-03-23 04:40:53 +0100

    add validation middleware

--- apps/server/src/modules/invitation/invitation.route.ts
+++ apps/server/src/modules/invitation/invitation.route.ts
@@ -1,11 +1,17 @@
 import express, { Router } from 'express';
 import { ensureAuth } from '@/modules/auth/auth.middleware';
 import { ensureInviteeMatch } from './invitation.middleware';
+import * as invitationController from './invitation.controller';
+import { validateRequest } from '../../lib/helpers';
+import * as invitationSchema from './invitation.validation'
 
 const invitationRouter: Router = express.Router();
 
 invitationRouter.patch("/:id/",
- ensureAuth, ensureInviteeMatch, 
+ ensureAuth,
+ ensureInviteeMatch,
+ validateRequest(invitationSchema.acceptDocumentInvitationSchema),
+ invitationController.acceptDocumentInvitation
 )
 
 export default invitationRouter;
--- apps/server/src/modules/invitation/invitation.schema.ts
+++
@@ -1,3 +0,0 @@
-import z from 'zod';
-import { Role } from '../../db/prisma/generated/types';
-
