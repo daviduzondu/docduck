b3f41077e6c5fd4a1359d6e4be51c8c22d461771
David Uzondu <uzondudavid2004@gmail.com>
2026-04-01 12:31:57 +0100

    complete top bar basics

--- apps/web/app/doc/[noteId]/page.client.tsx
+++ apps/web/app/doc/[noteId]/page.client.tsx
@@ -2,10 +2,17 @@
 
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
 import useHocuspocus from "../../hooks/use-hocuspocus";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import AuthGuard from "../../guards/auth.guard";
-import { Button } from "../../components/ui/button";
-
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import {
+ Avatar,
+ AvatarFallback,
+ AvatarGroup,
+ AvatarImage,
+} from "@/components/ui/avatar"
+import { Share2 } from 'lucide-react';
 
 export default function NotePage() {
  const { noteId }: { noteId: string } = useParams();
@@ -18,12 +25,29 @@ export default function NotePage() {
    {/* <div className="w-full text-center text-sm py-2">You're currently offline. Changes will sync automatically.</div> */}
    <header className="flex w-full items-center px-3 py-1 justify-between">
     <div className="text-2xl font-bold grow basis-0 ">DocDuck</div>
-    <div className="w-[25%] text-center">
-     <div className="truncate text-center">Quarterly Report (Q3 2024)</div>
+    <div className="w-[25%] text-center space-x-1">
+     <div className="truncate text-center inline">Quarterly Report (Q3 2024)</div>
+     <Badge className="uppercase text-xs inline" variant={'default'}>private</Badge>
     </div>
-    <div className="gap-2  items-center grow basis-0 text-right">
-     [collaborators]
-     <Button>Share</Button>
+    <div className="gap-2 items-center flex grow basis-0 justify-end">
+     <AvatarGroup className="">
+      <Avatar>
+       <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+       <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Avatar>
+       <AvatarImage src="https://github.com/maxleiter.png" alt="@maxleiter" />
+       <AvatarFallback>LR</AvatarFallback>
+      </Avatar>
+      <Avatar>
+       <AvatarImage
+        src="https://github.com/evilrabbit.png"
+        alt="@evilrabbit"
+       />
+       <AvatarFallback>ER</AvatarFallback>
+      </Avatar>
+     </AvatarGroup>
+     <Button size={'lg'}><Share2 data-icon="inline-end" />Share</Button>
     </div>
    </header>
    <AuthGuard next={"/doc/" + noteId}>
